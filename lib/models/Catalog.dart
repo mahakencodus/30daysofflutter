4959dc44433530896e0eb564d69e212efb7c1327
mahakencodus <81672161+mahakencodus@users.noreply.github.com>
2021-07-16 10:00:01 +0530

    day 6 commit

--- lib/models/Catalog.dart
+++ lib/models/Catalog.dart
@@ -12,12 +12,13 @@ class CatalogData {
       {this.id, this.name, this.desc, this.price, this.color, this.image});
 }
 
-final products = [CatalogData(
-  id: 1,
-  name: "Airpod",
-  desc: "Apple iphone 12th generation",
-  price: 25000,
-  color: '#33505a',
-  image: 'https://media.wired.com/photos/59e94ff8ce22fd0cca3c5242/master/w_2560%2Cc_limit/headphones-edit-1.jpg'
-
-)];
+final products = [
+  CatalogData(
+      id: 1,
+      name: "Airpod",
+      desc: "Apple iphone 12th generation",
+      price: 25000,
+      color: '#33505a',
+      image:
+          'https://media.wired.com/photos/59e94ff8ce22fd0cca3c5242/master/w_2560%2Cc_limit/headphones-edit-1.jpg')
+];
